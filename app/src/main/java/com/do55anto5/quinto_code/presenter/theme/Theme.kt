157b65b7f729418d6ba1d40c06b2b2d27c292907
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-11 19:00:01 -0300

    feat: add Color.kt and Theme.kt - determine which color to both light and dark themes

---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/theme/Theme.kt
@@ -0,0 +1,102 @@
+package com.do55anto5.quinto_code.presenter.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+private val LightColorScheme = MyColorScheme(
+    defaultColor = DefaultColor,
+    alphaDefaultColor = AlphaDefaultColor,
+    disabledDefaultColor = DisabledDefaultColor,
+    backgroundColor = BackgroundColorLight,
+    borderColor = BorderColorLight,
+    dividerColor = DividerColorLight,
+    topAppBarColor = TopAppBarColorLight,
+    textFieldBackGroundColor = TextFieldBackGroundColorLight,
+    backgroundSocialButtonColor = BackgroundSocialButtonColorLight,
+    textColor = TextColorLight,
+    buttonTextColor = ButtonTextColorLight,
+    secondaryButtonColor = SecondaryButtonColorLight,
+    secondaryButtonTextColor = SecondaryButtonTextColorLight,
+    successColor = SuccessColor,
+    errorColor = ErrorColor,
+    warningColor = WarningColor,
+    infoColor = InfoColor,
+    disabledColor = DisabledColor,
+    greyscale900Color = Greyscale900Color,
+    greyscale800Color = Greyscale800Color,
+    greyscale700Color = Greyscale700Color,
+    greyscale600Color = Greyscale600Color,
+    greyscale500Color = Greyscale500Color,
+    greyscale400Color = Greyscale400Color,
+    greyscale300Color = Greyscale300Color,
+    greyscale200Color = Greyscale200Color,
+    greyscale100Color = Greyscale100Color,
+    greyscale50Color = Greyscale50Color,
+    whiteColor = WhiteColor,
+    blackColor = BlackColor,
+    transparentColor = TransparentColor
+)
+
+private val DarkColorScheme = MyColorScheme(
+    defaultColor = DefaultColor,
+    alphaDefaultColor = AlphaDefaultColor,
+    disabledDefaultColor = DisabledDefaultColor,
+    backgroundColor = BackgroundColorDark,
+    borderColor = BorderColorDark,
+    dividerColor = DividerColorDark,
+    topAppBarColor = TopAppBarColorDark,
+    textFieldBackGroundColor = TextFieldBackGroundColorDark,
+    backgroundSocialButtonColor = BackgroundSocialButtonColorDark,
+    textColor = TextColorDark,
+    buttonTextColor = ButtonTextColorDark,
+    secondaryButtonColor = SecondaryButtonColorDark,
+    secondaryButtonTextColor = SecondaryButtonTextColorDark,
+    successColor = SuccessColor,
+    errorColor = ErrorColor,
+    warningColor = WarningColor,
+    infoColor = InfoColor,
+    disabledColor = DisabledColor,
+    greyscale900Color = Greyscale900Color,
+    greyscale800Color = Greyscale800Color,
+    greyscale700Color = Greyscale700Color,
+    greyscale600Color = Greyscale600Color,
+    greyscale500Color = Greyscale500Color,
+    greyscale400Color = Greyscale400Color,
+    greyscale300Color = Greyscale300Color,
+    greyscale200Color = Greyscale200Color,
+    greyscale100Color = Greyscale100Color,
+    greyscale50Color = Greyscale50Color,
+    whiteColor = WhiteColor,
+    blackColor = BlackColor,
+    transparentColor = TransparentColor
+)
+
+private val LocalColorScheme = compositionLocalOf { LightColorScheme }
+
+object QuintoCodeTheme {
+    val colorScheme: MyColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
+}
+
+@Composable
+fun QuintoCodeTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme by remember(isDarkTheme) {
+        mutableStateOf(if (isDarkTheme) DarkColorScheme else LightColorScheme)
+    }
+
+    CompositionLocalProvider(LocalColorScheme provides colorScheme) {
+        MaterialTheme(content = content)
+    }
+}
