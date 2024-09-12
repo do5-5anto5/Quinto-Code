b44b71af3d2bfbf0333a86554265b3fcbb118be7
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-12 16:51:37 -0300

    feat: change SplashScreen's package, implement LaunchedEffect inside it to intent to MainActivity

--- app/src/main/java/com/do55anto5/quinto_code/presenter/splash/screen/SplashScreen.kt
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/splash/screen/SplashScreen.kt
@@ -1,6 +1,10 @@
-package com.do55anto5.quinto_code.presenter.splashScreen
+package com.do55anto5.quinto_code.presenter.splash.screen
 
-
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -10,10 +14,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -21,8 +29,10 @@ import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.do55anto5.quinto_code.MainActivity
 import com.do55anto5.quinto_code.R
 import com.do55anto5.quinto_code.presenter.theme.QuintoCodeTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(modifier: Modifier = Modifier) {
@@ -31,6 +41,27 @@ fun SplashScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun SplashContent() {
+
+
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) {
+        // TODO: handle result
+    }
+
+    val alpha = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        alpha.animateTo(1f,
+            animationSpec = tween(durationMillis = 1500))
+        delay(3000)
+        val intent = Intent(context, MainActivity::class.java)
+        launcher.launch(intent)
+    }
+
 
     val composition by rememberLottieComposition(LottieCompositionSpec.Asset("loading.json"))
 
@@ -45,11 +76,12 @@ fun SplashContent() {
                     .padding(innerPadding)
                     .padding(vertical = 100.dp),
                 content = {
-//
+
                     Image(
                         painter = painterResource(id = R.drawable.ic_launcher_foreground),
                         contentDescription = null,
                         modifier = Modifier
+                            .alpha(alpha.value)
                             .clip(RoundedCornerShape(110.dp))
                             .align(Alignment.Center)
                             .border(
@@ -63,6 +95,7 @@ fun SplashContent() {
                     LottieAnimation(
                         composition = composition,
                         modifier = Modifier
+                            .alpha(alpha.value)
                             .size(120.dp)
                             .align(Alignment.BottomCenter),
                         iterations = LottieConstants.IterateForever,
