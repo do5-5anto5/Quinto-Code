53e36f0a7c951726e9fc4ffb152ee4eb6cc6ae44
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-18 14:42:14 -0300

    feat: Create data.remote layer SignupRepositoryImpl

---
+++ app/src/main/java/com/do55anto5/quinto_code/data/remote/SignupRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.do55anto5.quinto_code.data.remote
+
+import com.do55anto5.quinto_code.domain.remote.SignupRepository
+
+class SignupRepositoryImpl : SignupRepository {
+
+    override suspend fun register(email: String, password: String) {
+
+    }
+
+}
