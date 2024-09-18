cd07de183b51f28037cb3d6e8710c8ba785f9ed9
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-18 19:16:20 -0300

    feat: initial implementation of user creation with email and password at SignupRepositoryImpl

--- app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/authentication/SignupRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/authentication/SignupRepositoryImpl.kt
@@ -1,11 +1,24 @@
 package com.do55anto5.quinto_code.data.remote.repository.authentication
 
 import com.do55anto5.quinto_code.domain.remote.repository.authentication.SignupRepository
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.coroutines.suspendCoroutine
 
-class SignupRepositoryImpl : SignupRepository {
+class SignupRepositoryImpl(
+    private val auth: FirebaseAuth
+) : SignupRepository {
 
     override suspend fun register(email: String, password: String) {
-
+        return suspendCoroutine { continuation ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+//                        continuation.resumeWith(Result.success(Unit))
+                    } else {
+//                        continuation.resumeWith(Result.failure(task.exception ?: Exception("Unknown error")))
+                    }
+                }
+        }
     }
 
 }
