cc7016968bcfa3d45d35e49cd8605c4d9fd52e95
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-18 16:59:30 -0300

    feat: Set up useCaseModule and presenterModel to modules() inside MainApplication

--- app/src/main/java/com/do55anto5/quinto_code/core/activity/application/MainApplication.kt
+++ app/src/main/java/com/do55anto5/quinto_code/core/activity/application/MainApplication.kt
@@ -1,7 +1,9 @@
 package com.do55anto5.quinto_code.core.activity.application
 
 import android.app.Application
+import com.do55anto5.quinto_code.di.presenterModule
 import com.do55anto5.quinto_code.di.repositoryModule
+import com.do55anto5.quinto_code.di.useCaseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -14,7 +16,11 @@ class MainApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@MainApplication)
-            modules(repositoryModule)
+            modules(
+                repositoryModule,
+                useCaseModule,
+                presenterModule
+            )
         }
     }
 }
