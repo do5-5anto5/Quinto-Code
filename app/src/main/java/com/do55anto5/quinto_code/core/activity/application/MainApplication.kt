5e130c91ca0a61b9a93bf959e79140e876aaf820
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-18 06:38:26 -0300

    feat: Implement MainApplication

---
+++ app/src/main/java/com/do55anto5/quinto_code/core/activity/application/MainApplication.kt
@@ -0,0 +1,19 @@
+package com.do55anto5.quinto_code.core.activity.application
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules()
+        }
+    }
+}
