5b71a4c610487bddf7f69dd8f7ef1476cf2f5e51
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-18 14:43:43 -0300

    feat: set up repositoryModule into MainApplication

--- app/src/main/java/com/do55anto5/quinto_code/core/activity/application/MainApplication.kt
+++ app/src/main/java/com/do55anto5/quinto_code/core/activity/application/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.do55anto5.quinto_code.core.activity.application
 
 import android.app.Application
+import com.do55anto5.quinto_code.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,7 +14,7 @@ class MainApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@MainApplication)
-            modules()
+            modules(repositoryModule)
         }
     }
 }
