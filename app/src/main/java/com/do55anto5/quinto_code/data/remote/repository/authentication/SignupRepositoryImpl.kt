1db19decec0c9a9eb4695c9c1f7266a676aba075
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-19 12:32:23 -0300

    refactor: Simplify code calling FirebaseHelper inside SignupRepositoryImpl

--- app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/authentication/SignupRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/authentication/SignupRepositoryImpl.kt
@@ -1,16 +1,14 @@
 package com.do55anto5.quinto_code.data.remote.repository.authentication
 
+import com.do55anto5.quinto_code.core.helper.FirebaseHelper
 import com.do55anto5.quinto_code.domain.remote.repository.authentication.SignupRepository
-import com.google.firebase.auth.FirebaseAuth
 import kotlin.coroutines.suspendCoroutine
 
-class SignupRepositoryImpl(
-    private val auth: FirebaseAuth
-) : SignupRepository {
+class SignupRepositoryImpl : SignupRepository {
 
     override suspend fun register(email: String, password: String) {
         return suspendCoroutine { continuation ->
-            auth.createUserWithEmailAndPassword(email, password)
+           FirebaseHelper.getAuth().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         continuation.resumeWith(Result.success(Unit))
