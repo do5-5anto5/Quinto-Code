b0d881299fe73c38773a6ef3a307d41f992dbe1d
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-22 16:30:24 -0300

    fix - implement textColor inside FeedbackUI

--- app/src/main/java/com/do55anto5/quinto_code/presenter/components/snackbar/FeedBackUI.kt
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/components/snackbar/FeedBackUI.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -19,12 +20,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.do55anto5.quinto_code.R
 import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType
-import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType.*
+import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType.ERROR
+import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType.INFO
+import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType.SUCCESS
+import com.do55anto5.quinto_code.core.enums.feedback.FeedbackType.WARNING
 import com.do55anto5.quinto_code.presenter.theme.QuintoCodeTheme
 import com.do55anto5.quinto_code.presenter.theme.UrbanistFamily
 
@@ -42,13 +47,20 @@ fun FeedbackUI(
         ERROR -> QuintoCodeTheme.colorScheme.errorColor
     }
 
+    val textColor = when(type) {
+        SUCCESS -> QuintoCodeTheme.colorScheme.successTextColor
+        INFO -> QuintoCodeTheme.colorScheme.infoTextColor
+        WARNING -> QuintoCodeTheme.colorScheme.warningTextColor
+        ERROR -> QuintoCodeTheme.colorScheme.errorTextColor
+    }
+
     Card(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp),
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(
-            containerColor = defaultColor.copy(alpha = 0.2f)
+            containerColor = defaultColor.copy(alpha = 0.7f)
         ),
         content = {
             Row(
@@ -59,6 +71,8 @@ fun FeedbackUI(
                 content = {
 
                     Icon(
+                        modifier = Modifier
+                            .size(30.dp),
                         painter = painterResource(id = R.drawable.ic_info),
                         contentDescription = null,
                         tint = defaultColor
@@ -69,7 +83,9 @@ fun FeedbackUI(
                     Text(text = message,
                         style = TextStyle(
                             fontFamily = UrbanistFamily,
-                            color = defaultColor,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            color = textColor,
                             letterSpacing = 0.5.sp
                         ))
                 }
