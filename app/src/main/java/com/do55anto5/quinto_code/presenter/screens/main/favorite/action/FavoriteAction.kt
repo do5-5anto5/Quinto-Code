47fdf4da60287e543bcc86a4ee6cc45997f19466
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-30 20:49:36 -0300

    feat: Set up Favorite screen structure

---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/main/favorite/action/FavoriteAction.kt
@@ -0,0 +1,6 @@
+package com.do55anto5.quinto_code.presenter.screens.main.favorite.action
+
+
+sealed class FavoriteAction(
+
+)
---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/main/favorite/state/FavoriteState.kt
@@ -0,0 +1,6 @@
+package com.do55anto5.quinto_code.presenter.screens.main.favorite.state
+
+
+data class FavoriteState(
+    val isLoading: Boolean = true
+)
