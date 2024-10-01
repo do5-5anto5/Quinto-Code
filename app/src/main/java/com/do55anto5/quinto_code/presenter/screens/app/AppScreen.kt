d6209c109b5d6444315c9b11bd3532a8f3f571d9
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-01 09:19:26 -0300

    feat: Add AppScreen, where all the app core content will be shown to user

---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/app/AppScreen.kt
@@ -0,0 +1,21 @@
+package com.do55anto5.quinto_code.presenter.screens.app
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+
+@Composable
+fun AppScreen() {
+    AppContent()
+}
+
+@Composable
+fun AppContent() {
+
+}
+
+@PreviewLightDark
+@Composable
+private fun AppScreenPreview() {
+    AppContent()
+}
