5d8c0b59b48c2c4a2c3b470a0849bd0cfa0b944a
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-02 15:25:02 -0300

    feat: Implement WelcomeAction, WelcomeState and WelcomeViewModel - set states getWelcomeVisited  and isLoading - set SplashAction.OnNextScreen - handle them in SplashViewModel

---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/splash/action/SplashAction.kt
@@ -0,0 +1,5 @@
+package com.do55anto5.quinto_code.presenter.screens.splash.action
+
+sealed class SplashAction {
+    data object OnNextScreen: SplashAction()
+}
---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/splash/viewmodel/SplashViewModel.kt
@@ -0,0 +1,35 @@
+package com.do55anto5.quinto_code.presenter.screens.splash.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.do55anto5.quinto_code.core.preferences.AppPreferences
+import com.do55anto5.quinto_code.presenter.screens.splash.action.SplashAction
+import com.do55anto5.quinto_code.presenter.screens.splash.state.SplashState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SplashViewModel(
+    private val appPreferences: AppPreferences
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(SplashState())
+    val state = _state.asStateFlow()
+
+    fun submitAction(action: SplashAction) {
+        when (action) {
+            SplashAction.OnNextScreen -> {
+                getWelcomeVisited()
+            }
+        }
+    }
+
+    private fun getWelcomeVisited() {
+        _state.update { currentState ->
+            currentState.copy(
+                isWelcomeVisited = appPreferences.getWelcomeVisited(),
+                isLoading = false
+            )
+        }
+    }
+
+}
