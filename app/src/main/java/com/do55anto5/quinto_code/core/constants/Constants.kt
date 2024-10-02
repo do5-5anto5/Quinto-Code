3b0042306c95094ef4c7e95dfde4bc525b24cb4f
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-02 15:31:20 -0300

    feat: Add AppPreferences.kt and Constants.kt

---
+++ app/src/main/java/com/do55anto5/quinto_code/core/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.do55anto5.quinto_code.core.constants
+
+object SharedPreferencesKeys {
+    const val FILE_NAME = "DEFAULT_PREFERENCES"
+    const val WELCOME_VISITED = "WELCOME_VISITED"
+}
