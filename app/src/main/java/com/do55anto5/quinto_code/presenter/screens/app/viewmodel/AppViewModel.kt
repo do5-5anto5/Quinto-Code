53c65b2e5e11e696b97392ba6e349ad3e909468b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-02 13:01:13 -0300

    feat: implement AppViewModel to handle actions and states from AppScreen

---
+++ app/src/main/java/com/do55anto5/quinto_code/presenter/screens/app/viewmodel/AppViewModel.kt
@@ -0,0 +1,47 @@
+package com.do55anto5.quinto_code.presenter.screens.app.viewmodel
+
+import androidx.compose.material3.DrawerValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.do55anto5.quinto_code.presenter.screens.app.action.AppAction
+import com.do55anto5.quinto_code.presenter.screens.app.state.AppState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class AppViewModel : ViewModel() {
+    private val _state = MutableStateFlow(AppState())
+    val state: StateFlow<AppState> = _state.asStateFlow()
+
+    fun submitAction(action: AppAction) {
+        when (action) {
+            is AppAction.DrawerItemClicked -> handleDrawerItemClick(action.index)
+            is AppAction.DrawerStateChanged -> handleDrawerStateChange(action.isOpen)
+            AppAction.NavigateBack -> handleNavigateBack()
+        }
+    }
+
+    private fun handleDrawerItemClick(index: Int) {
+        viewModelScope.launch {
+            _state.update { currentState ->
+                currentState.copy(
+                    previousDrawerIndex = currentState.currentDrawerIndex,
+                    currentDrawerIndex = index,
+                    drawerStateValue = DrawerValue.Closed
+                )
+            }
+        }
+    }
+
+    private fun handleDrawerStateChange(isOpen: Boolean) {
+        _state.update {
+            it.copy(drawerStateValue = if (isOpen) DrawerValue.Open else DrawerValue.Closed)
+        }
+    }
+
+    private fun handleNavigateBack() {
+        // Implement
+    }
+}
