8487fbe1f803672e4e6e6888bd157bc45c8e1e9e
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-11-09 17:23:41 -0300

    refactor: return suspendCoroutine{} from User and Image repositories implementation

--- app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/image/ImageRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/image/ImageRepositoryImpl.kt
@@ -3,16 +3,27 @@ package com.do55anto5.quinto_code.data.remote.repository.image
 import com.do55anto5.quinto_code.core.helper.FirebaseHelper.Companion.getFirebaseStorage
 import com.do55anto5.quinto_code.core.helper.FirebaseHelper.Companion.getUserId
 import com.do55anto5.quinto_code.domain.remote.repository.image.ImageRepository
+import kotlin.coroutines.suspendCoroutine
 
 class ImageRepositoryImpl : ImageRepository {
+    private val storageRef = getFirebaseStorage().reference
+    private val imageRef = storageRef
+        .child(
+            "images/${System.currentTimeMillis()}${getUserId()}.jpg"
+        )
 
     override suspend fun saveImage(imageBytes: ByteArray?) {
-        val storageRef = getFirebaseStorage().reference
-        val imageRef = storageRef
-            .child(
-                "images/${System.currentTimeMillis()}${getUserId()}.jpg"
-            )
-
-        imageRef.let { imageRef.putBytes(imageBytes!!) }
+        return suspendCoroutine {
+            imageBytes?.let { imageBytes ->
+                imageRef.putBytes(imageBytes)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            it.resumeWith(Result.success(Unit))
+                        } else {
+                            it.resumeWith(Result.failure(task.exception!!))
+                        }
+                    }
+            }
+        }
     }
 }
--- app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/quinto_code/data/remote/repository/user/UserRepositoryImpl.kt
@@ -4,27 +4,36 @@ import com.do55anto5.quinto_code.core.helper.FirebaseHelper
 import com.do55anto5.quinto_code.core.helper.FirebaseHelper.Companion.getDatabase
 import com.do55anto5.quinto_code.domain.remote.model.User
 import com.do55anto5.quinto_code.domain.remote.repository.user.UserRepository
-import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.suspendCoroutine
 
 class UserRepositoryImpl : UserRepository {
 
+    private val uid = FirebaseHelper.getUserId()
+    private val userRef = getDatabase().collection("users").document(uid)
+
     override suspend fun saveUser(user: User) {
-        val uid = FirebaseHelper.getUserId()
-        uid.let {
-            val userRef = getDatabase().collection("users").document(it)
+        return suspendCoroutine {
             userRef.set(user)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        it.resumeWith(Result.success(Unit))
+                    } else {
+                        it.resumeWith(Result.failure(task.exception!!))
+                    }
+                }
         }
     }
 
     override suspend fun getUser(): User {
-        val uid = FirebaseHelper.getUserId()
-        uid.let {
-            val docRef = getDatabase().collection("users").document(uid)
-            val user = docRef.get()
-                .await()
-                .toObject(User::class.java)
-
-            return user ?: User()
+        return suspendCoroutine {
+            userRef.get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        it.resumeWith(Result.success(task.result.toObject(User::class.java) ?: User()))
+                    } else {
+                        it.resumeWith(Result.failure(task.exception!!))
+                    }
+                }
         }
 
     }
