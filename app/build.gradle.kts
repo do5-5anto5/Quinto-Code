3f2ba2fb58fb7ea61291b7098e14845081a9a046
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-11-12 06:19:43 -0300

    refactor: Remove camera associated dependencies, dependency injection, and permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,14 +100,6 @@ dependencies {
     // Icons
     implementation(libs.androidx.material.icons.extended)
 
-    // Camera
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.video)
-    implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.extensions)
-
     // Texts
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
