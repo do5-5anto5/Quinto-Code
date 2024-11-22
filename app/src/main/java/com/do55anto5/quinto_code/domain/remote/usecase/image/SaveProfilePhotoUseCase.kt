9a0661c003a2c2ef225fcc65271e7521788ac56e
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-11-22 17:31:21 -0300

    refactor: adjust save and get image use cases to image types

--- app/src/main/java/com/do55anto5/quinto_code/domain/remote/usecase/image/SaveProfilePhotoUseCase.kt
+++ app/src/main/java/com/do55anto5/quinto_code/domain/remote/usecase/image/SaveProfilePhotoUseCase.kt
@@ -1,12 +1,13 @@
 package com.do55anto5.quinto_code.domain.remote.usecase.image
 
+import com.do55anto5.quinto_code.core.enums.image.ImageType
 import com.do55anto5.quinto_code.domain.remote.repository.image.ImageRepository
 
 class SaveProfilePhotoUseCase(
     private val repository: ImageRepository
 ) {
     suspend operator fun invoke(
-        imageBytes: ByteArray?) {
-        repository.saveProfilePhoto(imageBytes)
+        imageBytes: ByteArray?, imageType: ImageType) {
+        repository.saveImage(imageBytes, imageType)
     }
 }
