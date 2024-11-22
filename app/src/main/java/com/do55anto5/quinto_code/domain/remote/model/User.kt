aa512122844d56b016613010b2a106d0a60353ef
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-11-21 21:01:29 -0300

    feat: adapt model User to have a nullable store reference

--- app/src/main/java/com/do55anto5/quinto_code/domain/remote/model/User.kt
+++ app/src/main/java/com/do55anto5/quinto_code/domain/remote/model/User.kt
@@ -1,9 +1,14 @@
 package com.do55anto5.quinto_code.domain.remote.model
 
+import com.google.firebase.Timestamp
+
 data class User(
     val name: String? = null,
     val surname: String? = null,
     val email: String? = null,
     val photo: String? = null,
-    val city: String? = null
+    val city: String? = null,
+    val storeRef: String? = null,
+    val isVendor: Boolean = false,
+    val createdAt: Timestamp = Timestamp.now()
 )
